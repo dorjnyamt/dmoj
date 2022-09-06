87a3c4b0e29181bef95563e37ec88fb2eb75c864
Dorjnyam Tumur-Ochir <dorjnyam.to@gmail.com>
2022-09-06 21:00:38 +0800

    ccc19j3

--- ch04/ccc19j3.py
+++ ch04/ccc19j3.py
@@ -4,13 +4,15 @@ for i in range(n):
     line = input()
     encoded = ""
     prev = line[0]
-    freq = 0
+    freq = int(0)
     for pos, char in enumerate(line):
-        if char != prev or pos == len(line) - 1:
-            encoded = encoded + f"{freq} {prev} "
+        if char != prev:
+            encoded = encoded + f"{int(freq)} {prev} "
             prev = char
             freq = 1
-        if char == prev:
+        elif char == prev:
             freq = freq + 1
+        if pos == len(line) - 1:
+            encoded = encoded + f"{int(freq)} {prev} "
 
     print(encoded.strip())
