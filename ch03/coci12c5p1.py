e6633477c5246ba8f2cfae103a12b005bac1207c
Dorjnyam Tumur-Ochir <dorjnyam.to@gmail.com>
2022-09-14 14:40:12 +0800

    fixed coci12c5p1

--- ch03/coci12c5p1.py
+++ ch03/coci12c5p1.py
@@ -1,22 +1,23 @@
-# FIXME: partial correctness 
+# FIXED: partial correctness 
 
-composition = input().split("|")
-print(composition)
+composition = input()
 
 a_minor = 0
 c_major = 0
-for note in composition:
-    if note[0] in "ADE":
-        a_minor = a_minor + 1
-    elif note[0] in "CFG":
-        c_major = c_major + 1
+for note in range(len(composition)):
+    prev = note - 1
+    if note == 0 or composition[prev] == "|":
+        if composition[note] in "ADE":
+            a_minor += 1
+        elif composition[note] in "CFG":
+            c_major += 1
 
 if a_minor > c_major:
     print("A-mol")
 elif a_minor < c_major:
     print("C-dur")
 else:
-    if composition[-1][0] in "A":
+    if composition[-1] == "A":
         print("A-mol")
-    else:
+    elif composition[-1] == "C":
         print("C-dur")
